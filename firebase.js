37c0f702f84d9b7b7da7630975eb6289215648b3
uveshgata <uveshgata@gmail.com>
2025-12-14 11:05:32 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,15 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+// firebase.js (module)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyBQMidBylxAZGuQvBmhyAoI2uYLIma9CNU",
-    authDomain: "acs-calculator-a4f89.firebaseapp.com",
-    projectId: "acs-calculator-a4f89",
-    storageBucket: "acs-calculator-a4f89.firebasestorage.app",
-    messagingSenderId: "546157095238",
-    appId: "1:546157095238:web:17d8c408126cd4ed759d47"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyBQMidBylxAZGuQvBmhyAoI2uYLIma9CNU",
+  authDomain: "acs-calculator-a4f89.firebaseapp.com",
+  projectId: "acs-calculator-a4f89",
+  storageBucket: "acs-calculator-a4f89.firebasestorage.app",
+  messagingSenderId: "546157095238",
+  appId: "1:546157095238:web:17d8c408126cd4ed759d47"
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-
-  // expose globally (important for simple multi-page app)
-  window.firebaseApp = app;
-  window.firebaseAuth = auth;
-</script>
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
